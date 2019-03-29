67f5d5f2a38f3b9e65a173427df4a0aa240d2fd1
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2019-03-29 14:47:54 -0500

    Update B.java

--- Taller5POO/src/mundo/B.java
+++ Taller5POO/src/mundo/B.java
@@ -1,21 +1,28 @@
 package mundo;
 
-public class B {
-	
+public class B
+{
 	private int varB;
 	private A mia;
 	
-	public B(int varB, A mia){
-		this.varB = varB;
+	public B(int varB, A mia)
+	{
+		this.varB = varB;		
 		this.mia = mia;
 	}
 
-	public int getVarB() {
+	public int getVarB() 
+	{
 		return varB;
 	}
 
-	public void setVarB(int varB) {
+	public void setVarB(int varB) 
+	{
 		this.varB = varB;
 	}
 
+	public int byPass()
+	{ 
+		return mia.getVarA();		
+	}
 }
